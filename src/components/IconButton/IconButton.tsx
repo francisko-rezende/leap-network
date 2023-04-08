ae32940907aecd9160d2746ee7cbd592ffcd1527
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-04-08 18:39:20 -0300

    feat: replace radix hidden component with tailwind class

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,5 +1,4 @@
 import * as I from "@/ui/icons";
-import { Root as VisuallyHidden } from "@radix-ui/react-visually-hidden";
 
 const icons = {
   edit: <I.Notepad />,
@@ -19,8 +18,8 @@ export const IconButton = ({
   hiddenTextDescription,
 }: IconButtonProps) => {
   return (
-    <button className="rounded-lg transition-colors hover:bg-leap-blue-500 hover:shadow-sm active:bg-leap-blue-600">
-      {icons[intent]} <VisuallyHidden>{hiddenTextDescription}</VisuallyHidden>
+    <button className="p-0.5 transition-opacity hover:opacity-80 active:opacity-95">
+      {icons[intent]} <span className="sr-only">{hiddenTextDescription}</span>
     </button>
   );
 };
