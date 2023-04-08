9383ba4aaa280a31c4bdf2de42420dc45787e3c5
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-04-07 22:04:33 -0300

    feat: create Button component

---
+++ src/components/Button/Button.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./Button";
+
+const meta: Meta<typeof Button> = {
+  title: "CodeLeap Network/Button",
+  component: Button,
+  tags: ["autodocs"],
+  argTypes: {
+    upperCase: {
+      control: "boolean",
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    intent: "primary",
+    children: "Button",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    intent: "secondary",
+    children: "Button",
+  },
+};
+
+export const Delete: Story = {
+  args: {
+    intent: "delete",
+    children: "Button",
+  },
+};
+
+export const Save: Story = {
+  args: {
+    intent: "save",
+    children: "Button",
+  },
+};
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+
+const buttonStyles = cva(
+  [
+    "ml-auto",
+    "w-[120px]",
+    "rounded-lg",
+    "py-[6px]",
+    "font-bold",
+    "text-white",
+    "border",
+    "transition-colors",
+  ],
+  {
+    variants: {
+      intent: {
+        primary: [
+          "bg-leap-blue-400",
+          "hover:bg-leap-blue-500",
+          "active:bg-leap-blue-600",
+        ],
+        secondary: [
+          "bg-white",
+          "text-black",
+          "border-leap-gray-400",
+          "hover:bg-gray-100",
+          "active:bg-gray-400",
+        ],
+        delete: [
+          "bg-leap-red-500",
+          "text-white",
+          "hover:bg-leap-red-600",
+          "active:bg-leap-red-700",
+        ],
+        save: [
+          "bg-leap-green-500",
+          "text-white",
+          "hover:bg-leap-green-600",
+          "active:bg-leap-green-700",
+        ],
+      },
+      upperCase: {
+        true: "uppercase",
+      },
+    },
+    defaultVariants: {
+      intent: "primary",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonStyles> {
+  children: React.ReactNode;
+}
+
+export const Button = ({
+  className,
+  intent,
+  children,
+  upperCase,
+  ...props
+}: ButtonProps) => (
+  <button
+    className={twMerge(buttonStyles({ intent, upperCase }), className)}
+    {...props}
+  >
+    {children}
+  </button>
+);
