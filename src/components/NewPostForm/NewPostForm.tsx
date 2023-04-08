272cbcf6e9315db3961ad6ed0a4fe6fbe09218ff
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-04-08 13:08:30 -0300

    feat: create NewPostForm component

---
+++ src/components/NewPostForm/NewPostForm.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Card } from "@/components/Card";
+import { Title } from "@/components/Title";
+import { TextField } from "@/components/TextField";
+import { Button } from "@/components/Button";
+
+export const NewPostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const fields = [
+    {
+      id: "title",
+      label: "Title",
+      tag: "input",
+      placeholder: "New post",
+      value: title,
+      setValue: setTitle,
+    },
+    {
+      id: "content",
+      label: "Content",
+      tag: "textarea",
+      placeholder: "New post's content",
+      value: content,
+      setValue: setContent,
+    },
+  ] as const;
+
+  const hasEmptyField = !content || !title;
+
+  return (
+    <Card tag="section">
+      <Title tag="h1" intent="dark">
+        Welcome to CodeLeap network!
+      </Title>
+      <form className="grid gap-4" onSubmit={(e) => e.preventDefault()}>
+        {fields.map((field) => (
+          <TextField key={field.id} {...field} />
+        ))}
+        <Button disabled={hasEmptyField}>Create</Button>
+      </form>
+    </Card>
+  );
+};
