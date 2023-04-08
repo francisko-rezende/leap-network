e138d9c734546da0207c98fa38d86384b9c1c233
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-04-08 09:38:38 -0300

    feat: create the Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { Title } from "@/components/Title";
+
+export const Header = () => {
+  return (
+    <header className="w-full bg-leap-blue-400 px-9 py-7">
+      <Title tag="h1" intent="light">
+        CodeLeap Network
+      </Title>
+    </header>
+  );
+};
