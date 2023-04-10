d9d8e86dc8a53ff6a3bfa64b3c75253ec0540744
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-04-10 07:44:17 -0300

    feat: create custom hooks for dispatching and selecting using redux

---
+++ src/hooks/useAppDispatch.ts
@@ -0,0 +1,5 @@
+import { AppDispatch } from "@/redux/store/store";
+import { useDispatch } from "react-redux";
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch;
