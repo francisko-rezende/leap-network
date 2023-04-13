11567bdc21dd2c56cdb8bb1b56529d9dc7e4b47b
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-04-13 03:50:08 -0300

    feat: create signup page

---
+++ src/pages/signup.tsx
@@ -0,0 +1,9 @@
+import { SignupForm } from "@/components/SignupForm";
+
+export default function Signup() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <SignupForm />
+    </main>
+  );
+}
