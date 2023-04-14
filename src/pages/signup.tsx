917a95ddbb43efa39b9e15237a7aa283d1007044
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-04-14 08:53:55 -0300

    feat: add meta tags and favico to project

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,12 +1,18 @@
 import { SignupForm } from "@/components/SignupForm";
 import { useCheckIfLoggedIn } from "@/hooks/useCheckIfLoggedIn";
+import Head from "next/head";
 
 export default function Signup() {
   useCheckIfLoggedIn();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <SignupForm />
-    </main>
+    <div>
+      <Head>
+        <title>CodeLeap Network</title>
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <SignupForm />
+      </main>
+    </div>
   );
 }
