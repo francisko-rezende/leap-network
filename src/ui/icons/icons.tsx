efe2756751b7878f74d5cf7ef960153038bf7a1f
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-04-14 07:43:33 -0300

    feat: add sign out icon to project

--- src/ui/icons/icons.tsx
+++ src/ui/icons/icons.tsx
@@ -38,3 +38,18 @@ export const Spinner = () => (
     />
   </svg>
 );
+
+export const SignOut = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4M16 17l5-5-5-5M21 12H9" />
+  </svg>
+);
