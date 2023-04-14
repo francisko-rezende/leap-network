8f1308e96d141c504b5ea62f8c5d16b4c5f63dfa
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-04-14 04:46:44 -0300

    feat: make it possible to programaticall close Dialog

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,22 +1,27 @@
 import * as RadixDialog from "@radix-ui/react-dialog";
+import { Dispatch, SetStateAction } from "react";
 
 type DialogProps = {
   trigger: React.ReactNode;
   children: React.ReactNode;
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
 };
 
-export const Dialog = ({ trigger, children }: DialogProps) => (
-  <RadixDialog.Root>
-    <RadixDialog.Trigger asChild>{trigger}</RadixDialog.Trigger>
+export const Dialog = ({ trigger, children, open, setOpen }: DialogProps) => {
+  return (
+    <RadixDialog.Root open={open} onOpenChange={setOpen}>
+      <RadixDialog.Trigger asChild>{trigger}</RadixDialog.Trigger>
 
-    <RadixDialog.Portal>
-      <RadixDialog.Overlay className=" fixed inset-0 z-20 bg-leap-overlay data-[state=open]:animate-overlayShow" />
-      <RadixDialog.Content
-        aria-describedby={undefined}
-        className="fixed left-[50%] top-[50%] z-20 grid max-h-[85vh] w-[90vw] max-w-[660px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow"
-      >
-        {children}
-      </RadixDialog.Content>
-    </RadixDialog.Portal>
-  </RadixDialog.Root>
-);
+      <RadixDialog.Portal>
+        <RadixDialog.Overlay className=" fixed inset-0 z-20 bg-leap-overlay data-[state=open]:animate-overlayShow" />
+        <RadixDialog.Content
+          aria-describedby={undefined}
+          className="fixed left-[50%] top-[50%] z-20 grid max-h-[85vh] w-[90vw] max-w-[660px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow"
+        >
+          {children}
+        </RadixDialog.Content>
+      </RadixDialog.Portal>
+    </RadixDialog.Root>
+  );
+};
