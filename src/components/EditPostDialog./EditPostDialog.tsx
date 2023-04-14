5b1c4eb3b2df3252788113d2852ac0cd986b86e5
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-04-14 05:55:06 -0300

    refactor: move edit post mutation logic to a new hook

--- src/components/EditPostDialog./EditPostDialog.tsx
+++ src/components/EditPostDialog./EditPostDialog.tsx
@@ -5,20 +5,14 @@ import { Title } from "@/components/Title";
 import { Button } from "@/components/Button";
 import { DialogButtonsWrapper } from "@/components/DialogButtonsWrapper";
 import { FormEvent, useState } from "react";
-import { TextField } from "../TextField";
-import { useMutation } from "@tanstack/react-query";
-import { axiosInstance } from "@/lib/axios";
-import { NewPostData } from "@/types/newPostData";
-import { appQueryClient } from "@/lib/tanstackQuery";
+import { TextField } from "@/components/TextField";
+import { useEditPost } from "@/hooks/useEditPost";
 
 type EditPostDialogProps = {
   originalTitle: string;
   originalContent: string;
   id: number;
 };
-
-type EditPostData = Omit<NewPostData, "username"> &
-  Pick<EditPostDialogProps, "id">;
 
 export const EditPostDialog = ({
   originalTitle,
@@ -27,6 +21,10 @@ export const EditPostDialog = ({
 }: EditPostDialogProps) => {
   const [title, setTitle] = useState(originalTitle);
   const [content, setContent] = useState(originalContent);
+  const [open, setOpen] = useState(false);
+
+  const editPostMutation = useEditPost({ setOpen });
+
   const fields = [
     {
       id: "title",
@@ -48,21 +46,9 @@ export const EditPostDialog = ({
 
   const hasEmptyField = content.length === 0 || title.length === 0;
 
-  const [open, setOpen] = useState(false);
-
-  const mutation = useMutation({
-    mutationFn: ({ id, title, content }: EditPostData) => {
-      return axiosInstance.patch(`/${id}/`, { title, content });
-    },
-    onSuccess: () => {
-      appQueryClient.invalidateQueries(["posts"]);
-      setOpen(false);
-    },
-  });
-
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    mutation.mutate({ id, title, content });
+    editPostMutation.mutate({ id, title, content });
   };
 
   return (
@@ -85,7 +71,10 @@ export const EditPostDialog = ({
           <RadixDialog.DialogClose asChild>
             <Button intent="secondary">Cancel</Button>
           </RadixDialog.DialogClose>
-          <Button disabled={hasEmptyField || mutation.isLoading} intent="save">
+          <Button
+            disabled={hasEmptyField || editPostMutation.isLoading}
+            intent="save"
+          >
             Save
           </Button>
         </DialogButtonsWrapper>
---
+++ src/hooks/useEditPost.ts
@@ -0,0 +1,25 @@
+import { axiosInstance } from "@/lib/axios";
+import { appQueryClient } from "@/lib/tanstackQuery";
+import { NewPostData } from "@/types/newPostData";
+import { useMutation } from "@tanstack/react-query";
+import { Dispatch, SetStateAction } from "react";
+
+type EditPostData = Omit<NewPostData, "username"> & { id: number };
+
+type UseEditPostProps = {
+  setOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+export const useEditPost = ({ setOpen }: UseEditPostProps) => {
+  const editPostMutation = useMutation({
+    mutationFn: ({ id, title, content }: EditPostData) => {
+      return axiosInstance.patch(`/${id}/`, { title, content });
+    },
+    onSuccess: () => {
+      appQueryClient.invalidateQueries(["posts"]);
+      setOpen(false);
+    },
+  });
+
+  return editPostMutation;
+};
