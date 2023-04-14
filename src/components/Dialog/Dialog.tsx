cbff9cabbf40cdfa2ecb0d26ecb4e71c1a25d1c2
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-04-13 22:31:58 -0300

    feat: adjust header and dialog z-index so they appear over PostList

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -10,10 +10,10 @@ export const Dialog = ({ trigger, children }: DialogProps) => (
     <RadixDialog.Trigger asChild>{trigger}</RadixDialog.Trigger>
 
     <RadixDialog.Portal>
-      <RadixDialog.Overlay className="fixed inset-0 bg-leap-overlay data-[state=open]:animate-overlayShow" />
+      <RadixDialog.Overlay className=" fixed inset-0 z-20 bg-leap-overlay data-[state=open]:animate-overlayShow" />
       <RadixDialog.Content
         aria-describedby={undefined}
-        className="fixed left-[50%] top-[50%] grid max-h-[85vh] w-[90vw] max-w-[660px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow"
+        className="fixed left-[50%] top-[50%] z-20 grid max-h-[85vh] w-[90vw] max-w-[660px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow"
       >
         {children}
       </RadixDialog.Content>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { Header } from "@/components/Header";
 import { NewPostForm } from "@/components/NewPostForm";
+import { PostList } from "@/components/PostList";
 import { Title } from "@/components/Title";
 
 export default function Index() {
   return (
     <div className="flex min-h-full flex-col">
       <Header
-        className="sticky top-0 mx-auto w-full max-w-[800px]"
+        className="sticky top-0 z-10 mx-auto w-full max-w-[800px]"
         intent="page"
       >
         <Title intent="light" tag="h1">
@@ -15,6 +16,7 @@ export default function Index() {
       </Header>
       <main className="mx-auto flex h-full w-full max-w-[800px] flex-1 flex-col gap-6 overflow-y-auto bg-white px-4 py-6 sm:px-6">
         <NewPostForm />
+        <PostList />
       </main>
     </div>
   );
