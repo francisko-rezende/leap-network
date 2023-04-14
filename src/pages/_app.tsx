5cacc36bf5b080918e31839f4c1e71837c0810ef
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-04-13 22:24:40 -0300

    feat: add react query devtools to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,14 @@ import "@/styles/globals.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <QueryClientProvider client={appQueryClient}>
         <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </Provider>
   );
