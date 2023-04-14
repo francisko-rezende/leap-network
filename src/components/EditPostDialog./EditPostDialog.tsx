2fee204d73275978fae5a5100c2fd23b3a5f991d
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-04-14 04:52:33 -0300

    feat: implement post editing

--- src/components/EditPostDialog./EditPostDialog.tsx
+++ src/components/EditPostDialog./EditPostDialog.tsx
@@ -4,18 +4,27 @@ import { Dialog } from "@/components/Dialog";
 import { Title } from "@/components/Title";
 import { Button } from "@/components/Button";
 import { DialogButtonsWrapper } from "@/components/DialogButtonsWrapper";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { TextField } from "../TextField";
+import { useMutation } from "@tanstack/react-query";
+import { axiosInstance } from "@/lib/axios";
+import { NewPostData } from "@/types/newPostData";
+import { appQueryClient } from "@/lib/tanstackQuery";
 
-type EditPostDialog = {
+type EditPostDialogProps = {
   originalTitle: string;
   originalContent: string;
+  id: number;
 };
+
+type EditPostData = Omit<NewPostData, "username"> &
+  Pick<EditPostDialogProps, "id">;
 
 export const EditPostDialog = ({
   originalTitle,
   originalContent,
-}: EditPostDialog) => {
+  id,
+}: EditPostDialogProps) => {
   const [title, setTitle] = useState(originalTitle);
   const [content, setContent] = useState(originalContent);
   const fields = [
@@ -39,8 +48,27 @@ export const EditPostDialog = ({
 
   const hasEmptyField = content.length === 0 || title.length === 0;
 
+  const [open, setOpen] = useState(false);
+
+  const mutation = useMutation({
+    mutationFn: ({ id, title, content }: EditPostData) => {
+      return axiosInstance.patch(`/${id}/`, { title, content });
+    },
+    onSuccess: () => {
+      appQueryClient.invalidateQueries(["posts"]);
+      setOpen(false);
+    },
+  });
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutation.mutate({ id, title, content });
+  };
+
   return (
     <Dialog
+      open={open}
+      setOpen={setOpen}
       trigger={<IconButton hiddenTextDescription="Edit item" intent="edit" />}
     >
       <RadixDialog.Title asChild>
@@ -48,7 +76,7 @@ export const EditPostDialog = ({
           Edit item
         </Title>
       </RadixDialog.Title>
-      <form className="mt-6 grid gap-6" onSubmit={(e) => e.preventDefault()}>
+      <form className="mt-6 grid gap-6" onSubmit={handleSubmit}>
         {fields.map((field) => (
           <TextField key={field.id} {...field} />
         ))}
@@ -57,7 +85,7 @@ export const EditPostDialog = ({
           <RadixDialog.DialogClose asChild>
             <Button intent="secondary">Cancel</Button>
           </RadixDialog.DialogClose>
-          <Button disabled={hasEmptyField} intent="save">
+          <Button disabled={hasEmptyField || mutation.isLoading} intent="save">
             Save
           </Button>
         </DialogButtonsWrapper>
