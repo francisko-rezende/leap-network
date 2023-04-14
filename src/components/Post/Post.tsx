bb4147e7cfdf70f65bdfcba4e039f01a6c7bac70
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-04-14 04:50:59 -0300

    feat: only render edit and delete buttons if user is the author of a post

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -6,6 +6,8 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { DeletePostDialog } from "../DeletePostDialog";
 import { EditPostDialog } from "../EditPostDialog./EditPostDialog";
 import { PostProps } from "@/types/postProps";
+import { useAppSelector } from "@/hooks/useAppSelector";
+import { RootState } from "@/redux/store/store";
 dayjs.extend(relativeTime);
 
 export const Post = ({
@@ -13,8 +15,13 @@ export const Post = ({
   username,
   created_datetime,
   content,
+  id,
 }: PostProps) => {
   const timeSincePublished = dayjs(created_datetime).fromNow();
+
+  const user = useAppSelector((state: RootState) => state.user.user);
+
+  const isAuthor = user === username;
 
   return (
     <Card tag="article" hasBorder hasTitle>
@@ -22,10 +29,16 @@ export const Post = ({
         <Title tag="h2" intent="light" className="flex-[3]">
           {title}
         </Title>
-        <span className="flex max-w-[88px] flex-1 justify-between">
-          <DeletePostDialog />
-          <EditPostDialog originalContent={content} originalTitle={title} />
-        </span>
+        {isAuthor && (
+          <span className="flex max-w-[88px] flex-1 justify-between">
+            <DeletePostDialog />
+            <EditPostDialog
+              id={id}
+              originalContent={content}
+              originalTitle={title}
+            />
+          </span>
+        )}
       </Header>
       <div className="grid gap-3">
         <p className="flex justify-between text-[1.125rem] text-leap-gray-700">
